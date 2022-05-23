f7b4d7b86001064b255ae5e553fc5812af797b77
sam <ytai1123@gmail.com>
2022-05-23 14:57:02 +0800

    enviroment checking...

--- demo0_env_check.py
+++ demo0_env_check.py
@@ -3,8 +3,10 @@ import os
 import tensorflow as tf
 import sklearn
 import numpy as np
+import pandas as pd
 print(sys.executable)
 print(os.getcwd())
 print(tf.__version__)
 print(sklearn.__version__)
 print(np.__version__)
+print(pd.__version__)
